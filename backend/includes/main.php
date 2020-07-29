5c2689bf28d69cb2a2c4c24908ffec275abfcd88
ralphmensah <ralphmensah123@gmail.com>
2020-07-29 23:00:31 +0000

    Added: Header

--- backend/includes/main.php
+++ backend/includes/main.php
@@ -5,10 +5,13 @@ if(isset($_POST['list'])){
     $body = $_POST['list'];
     // echo $body;
     DB::query("INSERT INTO `list_tb` (`body`) VALUES ('$body');");
+    header("location: ./");
     
 }
 // To-do: replace 1 with user id
 $todo =  DB::query("SELECT * FROM `list_tb` WHERE 1;");
+
+
 // print_r($todo);
 
 
