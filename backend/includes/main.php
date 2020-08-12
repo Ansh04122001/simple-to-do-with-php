62e3e833bfaae72c67552abcf3bc36b3432d9d09
ralphmensah <ralphmensah123@gmail.com>
2020-08-12 03:37:02 +0000

    updated:some pages

--- backend/includes/main.php
+++ backend/includes/main.php
@@ -6,15 +6,15 @@ if(isset($_POST['list'])){
     // echo $body;
     DB::query("INSERT INTO `list_tb` (`body`) VALUES ('$body');");
     header("location: ./");
-    
+
 }
-// To-do: replace 1 with user id
+// TODO:replace 1 with user id
 $todo =  DB::query("SELECT * FROM `list_tb` WHERE 1;");
 
 
 // print_r($todo);
 
 
- 
+
 
 ?>
--- index.php
+++ index.php
@@ -20,82 +20,58 @@ include "backend/includes/main.php";
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
     />
+
     <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css" />
+
   </head>
   <body>
+
+
     <nav class="navbar navbar-expand-sm navbar-dark bg-info">
-      <a class="navbar-brand" href="#"
-        ><img src="images\icons8_add.svg" width="40px"
-      /></a>
+      <span class="navbar-brand" >
+        <img src="images\icons8_add.svg" width="40px"/>
+      </span>
 
-
-     
-
-      <!-- <form class="form-inline navbar-nav ml-auto my-2 my-lg-0"> -->
-      <!-- <button class="btn btn-outline-secondary" type="submit">Logout</button> -->
-
-      <!-- <div class="dropdown ml-auto">
-        <a class="nav-link dropdown-toggle" id="dropdownId" href="#" data-toggle="dropdown">Dropdown</a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Login</a></li>
-        </ul>
-      </div> -->
-      <!-- <div class="collapse navbar-collapse" id="collapsibleNavId"></div>
-        <ul class="navbar-nav ml-auto ">
-            <li class="nav-item dropdown active">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" >Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdownId">
-                    <a class="dropdown-item" href="#">Log Out</a>
-                    <a class="dropdown-item" href="#">Profile</a>
-                </div>
-            </li>
-        </ul>
-     </div> -->
-     
-     
-     <div class="dropdown open">
-       <a class="btn btn-secondary dropdown-toggle" type="button" id="triggerId" data-toggle="dropdown" aria-haspopup="true"
-           aria-expanded="false">
+     <div class="dropdown">
+        <button onclick="myFunction()" class="dropbtn">
              Dropdown
-           </button>
-       <div class="dropdown-menu" aria-labelledby="triggerId">
-         <a class="dropdown-item" href="#">Action</a>
-         <a class="dropdown-item disabled" href="#">Disabled action</a>
+        </button>
+       <div id="myDropdown" class="dropdown-content">
+         <a href="#">Action</a>
+         <a href="#">Disabled action</a>
        </div>
      </div>
-
-
 
 
     </nav>
 
     <div class="container">
       <?php foreach ($todo as $todoItem) { ?>
-        <!-- # code... -->
+        <!-- # Container code goes here ... -->
         <div class="row">
-        <label>
-          <input type="checkbox"> <?=$todoItem['body'] ?>
-        </label>
-      </div>
-        <?php } ?>
-      
+          <label>
+            <input type="checkbox"> <?=$todoItem['body'] ?>
+          </label>
+        </div>
+        <?php }
+      ?>
     </div>
 
+<!-- input code block goes here-->
     <div class="mt-5 ml-4 mr-4">
       <form method="POST" action="">
         <div class="input-group">
-          <input
-            type="text"
-            class="form-control"
-            name="list"
-            placeholder="Enter To do list"
-            required
-          />
+            <input
+              type="text"
+              class="form-control"
+              name="list"
+              placeholder="Enter To do list"
+              required
+            />
           <div class="input-group-append">
-            <button class="btn btn-outline-success" name="submit" type="submit"> add
+              <button class="btn btn-outline-success" name="submit" type="submit"> add
               <!-- <i class="fa fa-search"></i> -->
-            </button>
+              </button>
           </div>
         </div>
       </form>
